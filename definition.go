403293354f5d8109159e0683e94ff725d77b218d
Igor Karatayev <igor.cwer@gmail.com>
2024-06-12 16:07:45 +0300

    Simplify parser

--- definition.go
+++ definition.go
@@ -1,36 +1,13 @@
 package stagparser
 
-// Definition is a struct tag value element
-type Definition interface {
-	// Name is a name of the definition
-	Name() string
-	// Attributes are attributes of the definition
-	Attributes() map[string]interface{}
-	// Attribute returns an attribute value and true if an attribute exists
-	Attribute(name string) (interface{}, bool)
+type Definition struct {
+	Name       string
+	Attributes map[string]any
 }
 
-type definition struct {
-	name       string
-	attributes map[string]interface{}
-}
-
-func newDefinition(name string, attributes map[string]interface{}) Definition {
-	return &definition{
-		name:       name,
-		attributes: attributes,
+func newDefinition(name string, attributes map[string]any) Definition {
+	return Definition{
+		Name:       name,
+		Attributes: attributes,
 	}
-}
-
-func (d *definition) Name() string {
-	return d.name
-}
-
-func (d *definition) Attributes() map[string]interface{} {
-	return d.attributes
-}
-
-func (d *definition) Attribute(name string) (interface{}, bool) {
-	v, ok := d.attributes[name]
-	return v, ok
 }
--- parser.go
+++ parser.go
@@ -1,38 +1,38 @@
 // Package stagparser provides a generic parser for golang struct tag.
 // stagparser can parse tags like the following:
 //
-//   * `validate:"required,length(min=1, max=10)"`
-//   * `validate:"max=10,list=[apple,'star fruits']"`
+//   - `validate:"required,length(min=1, max=10)"`
+//   - `validate:"max=10,list=[apple,'star fruits']"`
 //
 // tags are consists of 'definition'. 'definition' have 3 forms:
 //
-//  * name only: required
-//  * name with a single attribute: max=10
-//      * in this case, parse result is name="max", attributes={"max":10}
-//  * name with multiple attributes: length(min=1, max=10)
+//   - name only: required
+//   - name with a single attribute: max=10
+//   - in this case, parse result is name="max", attributes={"max":10}
+//   - name with multiple attributes: length(min=1, max=10)
 //
 // name and attribute must be a golang identifier.
 // An attribute value must be one of an int64, a float64, an identifier,
 // a string quoted by "'" and an array.
 //
-//  * int64: 123
-//  * float64: 111.12
-//  * string: 'ab\tc'
-//    * identifiers are interpreted as string in value context
-//  * array:  [1, 2, aaa]
+//   - int64: 123
+//   - float64: 111.12
+//   - string: 'ab\tc'
+//   - identifiers are interpreted as string in value context
+//   - array:  [1, 2, aaa]
 //
 // You can parse objects just call ParseStruct:
 //
-//  import "github.com/yuin/stagparser"
+//	import "github.com/yuin/stagparser"
 //
-//  type User struct {
-//    Name string `validate:"required,length(min=4,max=10)"`
-//  }
+//	type User struct {
+//	  Name string `validate:"required,length(min=4,max=10)"`
+//	}
 //
-//  func main() {
-//    user := &User{"bob"}
-//    definitions, err := stagparser.ParseStruct(user)
-//  }
+//	func main() {
+//	  user := &User{"bob"}
+//	  definitions, err := stagparser.ParseStruct(user)
+//	}
 package stagparser
 
 import (
@@ -93,9 +93,9 @@ func newParser(source string) *parser {
 	return p
 }
 
-func (p *parser) Parse(tag string) ([]Definition, error) {
+func (p *parser) Parse(tag string) (map[string]Definition, error) {
 	p.s.Init(strings.NewReader(tag))
-	result := []Definition{}
+	result := map[string]Definition{}
 	for {
 		tok := p.s.Scan()
 		switch tok {
@@ -112,16 +112,16 @@ func (p *parser) Parse(tag string) ([]Definition, error) {
 				arg := map[string]interface{}{
 					ident: value,
 				}
-				result = append(result, newDefinition(ident, arg))
+				result[ident] = newDefinition(ident, arg)
 			} else if p.s.Peek() == '(' {
 				p.s.Next()
 				arg, err := p.parseArgs()
 				if err != nil {
 					return nil, err
 				}
-				result = append(result, newDefinition(ident, arg))
+				result[ident] = newDefinition(ident, arg)
 			} else if p.s.Peek() == scanner.EOF {
-				result = append(result, newDefinition(ident, map[string]interface{}{}))
+				result[ident] = newDefinition(ident, map[string]interface{}{})
 			}
 		case ',':
 			// NOP
@@ -263,14 +263,14 @@ func (p *parser) parseArgs() (map[string]interface{}, error) {
 }
 
 // ParseTag parses a given tag value.
-func ParseTag(value string, name string) ([]Definition, error) {
+func ParseTag(value string, name string) (map[string]Definition, error) {
 	p := newParser(name)
 	return p.Parse(value)
 }
 
 // ParseStruct parses struct tags of given object. map key is a field name.
-func ParseStruct(obj interface{}, tag string) (map[string][]Definition, error) {
-	result := map[string][]Definition{}
+func ParseStruct(obj interface{}, tag string) (map[string]map[string]Definition, error) {
+	result := map[string]map[string]Definition{}
 	r := reflect.ValueOf(obj)
 	if r.Kind() == reflect.Ptr {
 		obj = r.Elem().Interface()
