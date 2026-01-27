5e47fd8ec1f6bf3c1893f8e8212aaaba0d0a448a
Igor Karatayev <igor.cwer@gmail.com>
2026-01-27 21:29:55 +0300

    updated tests

--- parser_test.go
+++ parser_test.go
@@ -22,47 +22,47 @@ func TestExampleSuccess(t *testing.T) {
 	if len(f1) != 6 {
 		t.Fatalf("field f1 should be parsed into 6 definitions but %d", len(f1))
 	}
-	abc := f1[0]
-	if abc.Name() != "abc" {
+	abc := f1["abc"]
+	if abc.Name != "abc" {
 		t.Fatalf("1st f1 defition should be 'abc'")
 	}
-	if len(abc.Attributes()) != 1 {
+	if len(abc.Attributes) != 1 {
 		t.Fatalf("'abc' should have one attribute")
 	}
-	if v, ok := abc.Attribute("abc"); !ok || v.(int64) != 1 {
+	if v, ok := abc.Attributes["abc"]; !ok || v.(int64) != 1 {
 		t.Fatalf("abc attribute should be 1(int64) but got %v(%T)", v, v)
 	}
 
-	def := f1[1]
-	if def.Name() != "def" {
+	def := f1["def"]
+	if def.Name != "def" {
 		t.Fatalf("2nd f1 defition should be 'def'")
 	}
-	if len(def.Attributes()) != 1 {
+	if len(def.Attributes) != 1 {
 		t.Fatalf("'def' should have one attribute")
 	}
-	if v, ok := def.Attribute("def"); !ok || v.(string) != "ghi" {
+	if v, ok := def.Attributes["def"]; !ok || v.(string) != "ghi" {
 		t.Fatalf("def attribute should be \"ghi\" but got %v(%T)", v, v)
 	}
 
-	jkl := f1[2]
-	if jkl.Name() != "jkl" {
+	jkl := f1["jkl"]
+	if jkl.Name != "jkl" {
 		t.Fatalf("3rd f1 definition should be 'jkl'")
 	}
-	if len(jkl.Attributes()) != 1 {
+	if len(jkl.Attributes) != 1 {
 		t.Fatalf("'jkl' should have one attribute")
 	}
-	if v, ok := jkl.Attribute("jkl"); !ok || v.(string) != "mno" {
+	if v, ok := jkl.Attributes["jkl"]; !ok || v.(string) != "mno" {
 		t.Fatalf("jkl attribute should be \"mno\" but got %v(%T)", v, v)
 	}
 
-	pkr := f1[3]
-	if pkr.Name() != "pkr" {
-		t.Fatalf("4th f1 defition should be 'pkr' but got %s", pkr.Name())
+	pkr := f1["pkr"]
+	if pkr.Name != "pkr" {
+		t.Fatalf("4th f1 defition should be 'pkr' but got %s", pkr.Name)
 	}
-	if len(pkr.Attributes()) != 1 {
+	if len(pkr.Attributes) != 1 {
 		t.Fatalf("'pkr' should have one attribute")
 	}
-	if v, ok := pkr.Attribute("pkr"); !ok {
+	if v, ok := pkr.Attributes["pkr"]; !ok {
 		t.Fatalf("pkr attribute should be exists")
 	} else {
 		v2, ok2 := v.([]interface{})
@@ -83,25 +83,25 @@ func TestExampleSuccess(t *testing.T) {
 		}
 	}
 
-	stu := f1[4]
-	if stu.Name() != "stu" {
+	stu := f1["stu"]
+	if stu.Name != "stu" {
 		t.Fatalf("5th f1 definition should be 'stu'")
 	}
-	if len(stu.Attributes()) != 2 {
+	if len(stu.Attributes) != 2 {
 		t.Fatalf("'stu' should have two attributes")
 	}
-	if v, ok := stu.Attribute("vwx"); !ok || v.(string) != "ccc" {
+	if v, ok := stu.Attributes["vwx"]; !ok || v.(string) != "ccc" {
 		t.Fatalf("vwx attribute should be \"ccc\" but got %v(%T)", v, v)
 	}
-	if v, ok := stu.Attribute("zzz"); !ok || v.(string) != "ddd" {
+	if v, ok := stu.Attributes["zzz"]; !ok || v.(string) != "ddd" {
 		t.Fatalf("zzz attribute should be \"ddd\" but got %v(%T)", v, v)
 	}
 
-	a1 := f1[5]
-	if a1.Name() != "a1" {
+	a1 := f1["a1"]
+	if a1.Name != "a1" {
 		t.Fatalf("6th f1 definition should be 'a1'")
 	}
-	if len(a1.Attributes()) != 0 {
+	if len(a1.Attributes) != 0 {
 		t.Fatalf("'a1' should not have attributes")
 	}
 
@@ -113,14 +113,14 @@ func TestExampleSuccess(t *testing.T) {
 		t.Fatalf("field f2 should be parsed into 1 definitions but %d", len(f2))
 	}
 
-	abd := f2[0]
-	if abd.Name() != "abd" {
+	abd := f2["abd"]
+	if abd.Name != "abd" {
 		t.Fatalf("1st f2 definition should be 'abd'")
 	}
-	if len(abd.Attributes()) != 1 {
+	if len(abd.Attributes) != 1 {
 		t.Fatalf("'abd' should have one attribute")
 	}
-	if v, ok := abd.Attribute("abd"); !ok || v.(string) != "\r\n'" {
+	if v, ok := abd.Attributes["abd"]; !ok || v.(string) != "\r\n'" {
 		t.Fatalf("zzz attribute should be \"\\r\\n'\" but got %v(%T)", v, v)
 	}
 }
